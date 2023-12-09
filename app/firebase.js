ed01209c029bc0603455e84d6ff5168100b05319
Homlessss <48959447+Homlessss@users.noreply.github.com>
2023-12-09 20:25:50 +0700

    update

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzsK3V9fH-UwTvrG7TV2utW5BubGGba-o",
+    authDomain: "webgame-8626a.firebaseapp.com",
+    projectId: "webgame-8626a",
+    storageBucket: "webgame-8626a.appspot.com",
+    messagingSenderId: "807541012437",
+    appId: "1:807541012437:web:aa5d35fe27a841bd206f5c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
